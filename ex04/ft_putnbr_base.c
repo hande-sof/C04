a186fc05ef142120c8d47fe76dda1fa8b1e81023
soilee <hande.sof@gmail.com>
2020-07-23 22:40:29 +0900

    ex04

--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -6,9 +6,27 @@
 /*   By: soilee <soilee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 18:41:35 by soilee            #+#    #+#             */
-/*   Updated: 2020/07/23 20:24:20 by soilee           ###   ########.fr       */
+/*   Updated: 2020/07/23 22:39:59 by soilee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int		ft_is_base(char *base)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = i + 1;
+	if (*(base + 0) == '\0' || *(base + 1) == '\0')
+		return (0);
+	while (*(base + i) != '\0')
+	{
+		if (*(base + i) == '+' || *(base + i) == '-')
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int		ft_str_len(char *base)
 {
@@ -22,5 +40,9 @@ int		ft_str_len(char *base)
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	base = ft_str_len(base);
+	int base_n;
+
+	base_n = ft_str_len(base);
+	if (!ft_is_base(base))
+		return ;
 }
